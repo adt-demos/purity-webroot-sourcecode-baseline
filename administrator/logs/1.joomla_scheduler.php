edd63364ae7242527dda4cc2b27a4b2b0ade03d5
Templated User <templateduser@dummyemailX123.com>
2024-07-26 07:21:10 +0100

    Baseline Baby

---
+++ administrator/logs/1.joomla_scheduler.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-07-26 06:15:19 UTC
+#Software: Joomla! 5.1.1 Stable [ Kudumisha ] 28-May-2024 16:00 GMT
+
+#Fields: date	time	priority	message
+2024-07-26	06:15:19	INFO	Running task#03 'Update Notification'.
+2024-07-26	06:15:19	INFO	Task> Could not instantiate mail function.
+2024-07-26	06:15:19	INFO	Task> UpdateNotification end
+2024-07-26	06:15:19	INFO	Successfully finished task#03 in 0.29 (net 0.29) seconds.
+2024-07-26	06:15:33	INFO	Running task#02 'Session GC'.
+2024-07-26	06:15:33	INFO	Task> SessionGC end
+2024-07-26	06:15:33	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2024-07-26	06:15:50	INFO	Running task#01 'Rotate Logs'.
